9f48b2039fb8f8f3083811249cf201b35db6e2cd
lngach <nam.le@gumiviet.com>
2020-06-16 17:05:15 +0700

    Deploy

--- provider/provider.js
+++ provider/provider.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql')
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'cms',
+  host: 'remotemysql.com',
+  port: 3306,
+  user: 'fSncS4Zjuv',
+  password: 'c9sFrq5RGN',
+  database: 'fSncS4Zjuv',
 })
 con.connect(function (err) {
   if (err) throw err
