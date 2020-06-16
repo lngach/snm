310d9d8c2104638faf4888c155e57fbc9790982d
lngach <nam.le@gumiviet.com>
2020-06-16 17:20:05 +0700

    Update db

--- provider/provider.js
+++ provider/provider.js
@@ -3,9 +3,9 @@ const mysql = require('mysql')
 const con = mysql.createConnection({
   host: 'remotemysql.com',
   port: 3306,
-  user: 'root',
-  password: '',
-  database: 'cms',
+  user: 'fSncS4Zjuv',
+  password: 'c9sFrq5RGN',
+  database: 'fSncS4Zjuv',
 })
 con.connect(function (err) {
   if (err) throw err
