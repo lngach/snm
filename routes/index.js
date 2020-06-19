e27f63e7c33316e87a3368dae5d946a02efd919b
lngach <nam.le@gumiviet.com>
2020-06-19 16:39:18 +0700

    Update sitemap.xml

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res, next) {
 })
 
 router.get('/sitemap.xml', (_, res) => {
-  res.sendFile(__dirname + '/sitemap.txt')
+  res.sendFile('./../sitemap.txt')
 })
 
 module.exports = router
