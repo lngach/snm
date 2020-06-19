8f13b27f3b27ea02a1681d993bd79cd7a3f5955f
lngach <nam.le@gumiviet.com>
2020-06-19 21:31:44 +0700

    Update link

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express')
 var router = express.Router()
 var path = require('path')
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/', function (_, res) {
+  res.render('index', { title: 'SNM Technology' })
+})
+
+router.get('/about', function (_, res) {
+  res.render('index', { title: 'SNM Technology' })
+})
+
+router.get('/privacy', function (_, res) {
+  res.render('index', { title: 'SNM Technology' })
 })
 
 router.get('/sitemap.xml', (_, res) => {
